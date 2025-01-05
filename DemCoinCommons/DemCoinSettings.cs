ab84ccf4d34195dd4b0f2be74d62245b92b998e7
copokbl <copokbl@serble.net>
2025-01-05 19:51:30 +1100

    settings for prod

--- DemCoinCommons/DemCoinSettings.cs
+++ DemCoinCommons/DemCoinSettings.cs
@@ -2,7 +2,7 @@ namespace DemCoinCommons;
 
 public class DemCoinSettings {
     // 23 is very fast, 32 is reasonable for prod
-    public const int Difficulty = 23;  // Number of leading zeroes (bits) required in hash
+    public const int Difficulty = 33;  // Number of leading zeroes (bits) required in hash
     public const double MinerReward = 1;  // Coins rewarded for mining one block
     public static double AverageHashesToBlock() => Math.Pow(2, Difficulty);  // Average amount of tries before mining one block
 }
